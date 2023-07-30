9e66a115116b03be881df4ede6bdef60028687ba
Brian Rinaldi <brian.rinaldi@gmail.com>
2023-07-30 18:12:02 -0400

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind()],
+  integrations: [tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+  },
 });
